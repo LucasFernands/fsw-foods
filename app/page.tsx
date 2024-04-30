4a0cfed3b341058a52af6fb0d1c28b548601ebfb
Lucas Fernandes <lucasce.18@gmail.com>
2024-04-30 14:23:36 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return (
-    <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      ...
-    </button>
-  );
+  return <Header />;
 };
 
 export default Home;
