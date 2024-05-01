95dd871e942fe3310225ba8bfeaa5bbcd507d01e
Lucas Fernandes <lucasce.18@gmail.com>
2024-05-01 10:37:06 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -130,13 +130,9 @@ const ProductDetails = ({
               <TimerIcon size={14} />
             </div>
 
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-xs font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className="text-xs font-semibold">Grátis</p>
-            )}
+            <p className="text-xs font-semibold">
+              {product.restaurant.deliveryTimeMinutes} min
+            </p>
           </div>
         </Card>
       </div>
@@ -149,6 +145,10 @@ const ProductDetails = ({
       <div className="mt-6 space-y-3">
         <h3 className="px-5 font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
+      </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
       </div>
     </div>
   );
